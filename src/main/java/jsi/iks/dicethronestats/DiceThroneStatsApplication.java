7eb4c6896638eb3295a2d7565b9ceb31b239c794
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2024-11-26 09:15:12 +0100

    initial commit

---
+++ src/main/java/jsi/iks/dicethronestats/DiceThroneStatsApplication.java
@@ -0,0 +1,13 @@
+package jsi.iks.dicethronestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceThroneStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceThroneStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jsi/iks/dicethronestats/DiceThroneStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package jsi.iks.dicethronestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceThroneStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
